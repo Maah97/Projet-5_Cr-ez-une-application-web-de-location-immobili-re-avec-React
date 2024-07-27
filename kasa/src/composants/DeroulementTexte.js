b92c1451e0e82fb75a7611883c9ba9fe77e10d13
Maah97 <mahmoudouaboul@gmail.com>
2024-07-27 12:16:21 +0100

    Mise en place de la page d'à-propos

--- kasa/src/composants/DeroulementTexte.js
+++ kasa/src/composants/DeroulementTexte.js
@@ -1,8 +1,36 @@
-function DeroulementTexte(){
+import React, { useState, useEffect } from 'react'
+
+function DeroulementTexte(props){
+    const [open, setOpen] = useState(false);
+    let Class = 'paragraphe';
+    const [afficher, setAfficher] = useState(open);
+
+    useEffect(() => {
+        if (open===true) {
+           setAfficher(true);
+        } else {
+            const timer = setTimeout(() => {
+                setAfficher(false)
+            }, 300);
+            return () => {
+                clearTimeout(timer);
+            }
+        }
+    }, [open])
+
+    if (!open) {
+        Class += ' anim';
+    } 
+    // else {
+    //     Class += ' anim';
+    // }
     return (
-        <div className="derouleur-texte">
-            <p> <div className="fleche-du-haut"></div><div className="fleche-du-bas"></div></p>
-            <p className="paragraphe"></p>
+        <div className="les-apropos">
+            <div className="titre">
+                <p>{props.titre}</p>
+                <i onClick={() => setOpen(o => !o)} class="fa-solid fa-chevron-down"></i>
+            </div>
+            <p className={Class}>{afficher && props.contenu}</p>
         </div>
     )
 }
--- kasa/src/pages/Apropos.js
+++ kasa/src/pages/Apropos.js
@@ -1,10 +1,25 @@
 import BannerTitreHome from '../composants/BannerTitreHome';
+import DeroulementTexte from '../composants/DeroulementTexte'
 import "../styles/App.scss";
 
 function Apropos(){
+    const nbrApropos = [
+        {id:'0', titre:'Fiabilité', contenu:'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.'},
+        {id:'1', titre:'Respect', contenu:'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'},
+        {id:'2', titre:'Service', contenu:'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'},
+        {id:'3', titre:'Sécurité', contenu:'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l\'hôte qu\'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.'},
+    ];
     return (
         <section className='a-propos'>
             <BannerTitreHome />
+            <div className="bloc-des-apropos">
+                {
+                    nbrApropos.map((apropos) => (
+                        <DeroulementTexte key={apropos.id} titre={apropos.titre} contenu={apropos.contenu} />
+                    ))
+                }
+            </div>
+
         </section>
     )
 }
